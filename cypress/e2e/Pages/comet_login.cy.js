b75af668035072d9c7ff62230611cc5c26b583c6
Tester1 <137996855+tester-24@users.noreply.github.com>
2024-09-27 11:17:02 +0530

    Comet_Reports

--- cypress/e2e/Pages/comet_login.cy.js
+++ cypress/e2e/Pages/comet_login.cy.js
@@ -2,6 +2,7 @@ export class LoginPage {
 
     enterUsername() {
         //cy.get(':nth-child(2) > .text_form').clear()
+        cy.wait(1000)
         cy.get('.userid_wrap > .input_wrap > .text_form').type('M3903')
         
     }
