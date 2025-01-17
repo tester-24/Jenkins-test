37e3ab3dacfc40bb8fcf40066dafcbb77a2389fe
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-01-17 11:28:25 +0530

    Old_Comet_Reports

--- cypress/e2e/Pages/Report_GLobal_Report.cy.js
+++ cypress/e2e/Pages/Report_GLobal_Report.cy.js
@@ -40,21 +40,34 @@ loginPage.clickLogin();
 
 //Enter Pin
 cy.wait(2000);
-cy.get('[formcontrolname="otp1"]').type('1');
-cy.get('[formcontrolname="otp2"]').type('2');
-cy.get('[formcontrolname="otp3"]').type('3');
-cy.get('[formcontrolname="otp4"]').type('4');
-//cy.wait(3000);
- 
- //cy.get('.continue_button_wrap > p').click()
- //cy.wait(3000)
- //cy.get('.continue_button_wrap > p').click()
- //cy.wait(5000)
+cy.get('#pin1').type(1)
+cy.get('#pin2').type(2)
+cy.get('#pin3').type(3)
+cy.get('#pin4').type(4)
+cy.wait(5000)
+// cy.get('.payin-btn > .ng-star-inserted').invoke("removeAttr", "target").click()
+ cy.window().then((win) => {
+     cy.stub(win, 'open').callsFake((url) => {
+       win.location.href = url;
+     });
+   });
+   cy.wait(2000)
+   cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+   
+   // Verify that the page or URL is updated
+   cy.wait(4000)
+   cy.url().should('include', 'https://comet.jainam.in/#/corporate-actions');
+   Cypress.on('uncaught:exception', (err) => {
+     // returning false here prevents Cypress from
+     // failing the test
+     console.log('Cypress detected uncaught exception: ', err);
+     return false;
+   });
+// token link
+//cy.visit('https://uatcomet.jainam.in/#/RedirectToComet/jplus/?6r3+QKbMUDkiMZAdAPLpzYQAAMfXAxRTXy3TwJL/ij2+8yniWqsQN5CxIMcT082BZ79LFbKrXcoNjJLaGLVG0jieh0WNPDsse3ShhS4iRV2Z9q2mTPzOve9U2JSh4DkP')
+//cy.wait(500)
+// cy.xpath('/html/body/app-root/app-client-header-navbar/div[1]/div/nav/div[2]/ul/li[3]').click() 
 
-
-// //Click on form
-// cy.get('#AddNomineeDetail > .modal-dialog > .modal-content > .modal-body > .close').click({ force: true })
-// cy.wait(3000);
 
     // click on Reports
     cy.wait(4000)
@@ -69,8 +82,8 @@ cy.get('[formcontrolname="otp4"]').type('4');
     // cy.wait(3000)
 
     //Selecting EQUITY
-    cy.wait(2000)
-    cy.get('.container > form.ng-valid > :nth-child(2) > :nth-child(1)').click()
+    //cy.wait(2000)
+    cy.get('.container > form.ng-valid > :nth-child(2) > :nth-child(1)').click({force:true})
     
     //Click on DOWNLOAD BY 
    cy.wait(1000)
@@ -102,6 +115,14 @@ cy.get('[formcontrolname="otp4"]').type('4');
 
     //Download 
     cy.wait(3000)
+     // logout flow
+      // click on user profile 
+      cy.get('.user-icon').click({force:true})
+      cy.wait(1500)
+      // click on logout 
+     cy.xpath('/html/body/app-root/app-layout/app-headerpanel/div/div/nav/div/ul/li[5]/div/div/div/div[2]/ul/li[5]/a').click({force:true})
+      // cy.get('.mt-4 > :nth-child(4)').click({force:true})
+      cy.wait(1000)
    // cy.get('.col-lg-2 > .btn').click()
    // cy.wait(9000)
 
