70906ab7efb7be6c8dbee0d9b7b9c52d21147a06
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-01-17 11:32:19 +0530

    Old_Comet_Reports

--- cypress/e2e/Pages/login_1.cy.js
+++ cypress/e2e/Pages/login_1.cy.js
@@ -2,14 +2,19 @@ export class LoginPage {
 
     enterUsername() {
         //cy.get(':nth-child(2) > .text_form').clear()
-        cy.get('.userid_wrap > .input_wrap > .text_form').type('1126')
+        cy.wait(1500)
+        cy.xpath('/html[1]/body[1]/app-root[1]/app-sign-in[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/kendo-textbox[1]/input[1]').clear()
+        .type('1126')
+                   
+        //click on continue button
+        cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/div/form/button[1]').click()
     }
 
     enterPassword() {
-        cy.get('#LoginPassword').type('Jainam@123')
+        cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/app-sign-in-with-username/form/div/div/div/div/kendo-textbox/input').type('Jainam@123')
     }
 
     clickLogin() {
-        cy.xpath("//p[normalize-space()='Login Now']").click({ force: true });
+        cy.xpath('/html/body/app-root/app-sign-in/div/div/div[2]/app-sign-in-with-username/form/div/button').click()
     }
 }
