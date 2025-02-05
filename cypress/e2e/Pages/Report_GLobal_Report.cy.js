67b78629f20005e6132524ca28cac1c52da4b34e
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-02-05 11:45:45 +0530

    Old_Comet_Reports

--- cypress/e2e/Pages/Report_GLobal_Report.cy.js
+++ cypress/e2e/Pages/Report_GLobal_Report.cy.js
@@ -44,26 +44,26 @@ cy.get('#pin1').type(1)
 cy.get('#pin2').type(2)
 cy.get('#pin3').type(3)
 cy.get('#pin4').type(4)
-cy.wait(5000)
+cy.wait(3000)
 // cy.get('.payin-btn > .ng-star-inserted').invoke("removeAttr", "target").click()
-cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").click({force:true})
-     // cy.wait(500)
-      cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
-      cy.wait(500)
-      cy.window().then((win) => {
-             cy.stub(win, 'open').callsFake((url) => {
-               win.location.href = url;
-             });
-           });
-           cy.wait(2000)
-        //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
-      cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[3]/a[1]').click({force:true})
+// cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").invoke('show').click({force:true})
+//      // cy.wait(500)
+//       cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
+//       cy.wait(500)
+//       cy.window().then((win) => {
+//              cy.stub(win, 'open').callsFake((url) => {
+//                win.location.href = url;
+//              });
+//            });
+//            cy.wait(2000)
+//         //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+//       cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[3]/a[1]').click({force:true})
 
-
-    // // 4) Global Report
-    // cy.get('#ReportDropdown').click()
-    // cy.get('[href="#/pnl"]').click()
-    // cy.wait(3000)
+    // click on report and select global report
+    // 4) Global Report
+    cy.get('#ReportDropdown').click()
+    cy.get('[href="#/pnl"]').click()
+    cy.wait(3000)
 
     //Selecting EQUITY
     //cy.wait(2000)
