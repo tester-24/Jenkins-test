ad273ae268aaf4944c7867785110f9c68b543bd6
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-02-05 11:48:19 +0530

    Old_Comet_Reports

--- cypress/e2e/Pages/Report_Tax_Report.cy.js
+++ cypress/e2e/Pages/Report_Tax_Report.cy.js
@@ -21,19 +21,24 @@ it('Comet_Jainam', () => {
     cy.get('#pin2').type(2)
     cy.get('#pin3').type(3)
     cy.get('#pin4').type(4)
-    cy.wait(5000)
-    cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").click({force:true})
-    // cy.wait(500)
-     cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
-     cy.wait(500)
-     cy.window().then((win) => {
-            cy.stub(win, 'open').callsFake((url) => {
-              win.location.href = url;
-            });
-          });
-          cy.wait(1000)
-       //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
-     cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[5]/a[1]').click({force:true})
+    cy.wait(3000)
+     // click on report and select tax p and l  report
+    
+    cy.get('#ReportDropdown').click({force:true})
+    cy.get('[href="#/tax"]').click({force:true})
+    cy.wait(2000)
+    // cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").click({force:true})
+    // // cy.wait(500)
+    //  cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").should('be.visible')
+    //  cy.wait(500)
+    //  cy.window().then((win) => {
+    //         cy.stub(win, 'open').callsFake((url) => {
+    //           win.location.href = url;
+    //         });
+    //       });
+    //       cy.wait(1000)
+    //    //  cy.xpath('/html/body/app-root/app-layout/div/div/app-dashboard/div[2]/div/app-invest-with-us/div/a[4]').click()
+    //  cy.xpath('/html[1]/body[1]/app-root[1]/app-layout[1]/app-client-header-navbar[1]/div[1]/div[1]/nav[1]/div[2]/ul[1]/li[3]/div[1]/ul[1]/li[5]/a[1]').click({force:true})
 
     //Select FY Year
      cy.get('span.k-input-value-text').eq(1).click({ force: true }).type("{downArrow}{Enter}")
