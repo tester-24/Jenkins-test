dd44ed464e79fbca72175d799c3f6ecafed084d3
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-02-11 13:17:33 +0530

    Old_Comet_Reports

--- cypress/e2e/Pages/Report_GLobal_Report.cy.js
+++ cypress/e2e/Pages/Report_GLobal_Report.cy.js
@@ -44,7 +44,7 @@ it('Comet_Jainam', () => {
     cy.get('#pin2').type(2)
     cy.get('#pin3').type(3)
     cy.get('#pin4').type(4)
-    cy.wait(3000)
+    cy.wait(7000)
    // cy.get('.payin-btn > .ng-star-inserted').invoke("removeAttr", "target").click()
     cy.xpath("//a[@class='nav_title text_decoration_none d_inline_block']//img[@alt='reports']").invoke('show').click({force:true})
          // cy.wait(500)
